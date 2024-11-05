22b9d894ba9795e70aeec7813f8f2c1dea986efa
Mizu231 <lupitaherrera8491@gmail.com>
2024-11-05 10:10:33 -0600

    Add files via upload

---
+++ sensor.ino
@@ -0,0 +1,39 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+//#include <Adafruit_Sensor.h>
+
+int led = 3;
+int SENSOR = 2;
+int temperatura;
+int humedad;
+
+DHT dht (SENSOR, DHT11);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+temperatura = dht.readTemperature();
+humedad = dht.readHumidity();
+delay(1);
+Serial.print("Temperatura : ");
+Serial.print(temperatura);
+Serial.print(" Humedad: ");
+Serial.println (humedad);
+delay(500);
+
+if(temperatura >=28){
+  digitalWrite(led,HIGH);
+  }
+
+}
